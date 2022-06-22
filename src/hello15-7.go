86c466d2ae76f09f70fd5c9dda460a259d589482
he ping <847050412@qq.com>
2022-06-22 23:26:14 +0800

    接口

--- src/hello15-7.go
+++ src/hello15-7.go
@@ -1,32 +1,33 @@
 package main
 
-import "fmt"
-
-/*
-2.1.7 选择器的冷知识
-
-*/
-
-//从一个结构体实例对象中获取字段的值，通常都是使用 . 这个操作符，该操作符叫做 选择器。
 //
-//选择器有一个妙用，可能大多数人都不清楚。
+//import "fmt"
 //
-//当你对象是结构体对象的指针时，你想要获取字段属性时，按照常规理解应该这么做
-
-type Profile struct {
-	Name string
-}
-
-func main() {
-	//指针变量
-	p1 := &Profile{"iswbm"}
-	fmt.Println((*p1).Name) // output: iswbm
-	p1.Name = "weibo"
-	fmt.Println(p1)
-	//&{weibo}
-
-	//但还有一个更简洁的做法，可以直接省去 * 取值的操作，选择器 . 会直接解引用，示例如下
-
-	p2 := &Profile{"cnm"}
-	fmt.Println(p2.Name) // output: cnm
-}
+///*
+//2.1.7 选择器的冷知识
+//
+//*/
+//
+////从一个结构体实例对象中获取字段的值，通常都是使用 . 这个操作符，该操作符叫做 选择器。
+////
+////选择器有一个妙用，可能大多数人都不清楚。
+////
+////当你对象是结构体对象的指针时，你想要获取字段属性时，按照常规理解应该这么做
+//
+//type Profile struct {
+//	Name string
+//}
+//
+//func main() {
+//	//指针变量
+//	p1 := &Profile{"iswbm"}
+//	fmt.Println((*p1).Name) // output: iswbm
+//	p1.Name = "weibo"
+//	fmt.Println(p1)
+//	//&{weibo}
+//
+//	//但还有一个更简洁的做法，可以直接省去 * 取值的操作，选择器 . 会直接解引用，示例如下
+//
+//	p2 := &Profile{"cnm"}
+//	fmt.Println(p2.Name) // output: cnm
+//}
--- src/hello16-1.go
+++ src/hello16-1.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 /*
 2.2.1 面向对象:接口与多态
 在面向对象的领域里，接口一般这样定义：接口定义一个对象的行为。接口只指定了对象应该做什么，至于如何实现这个行为（即实现细节），则由对象本身去确定。
@@ -12,6 +17,115 @@ package main
 //
 //如下代码，定义了一个电话接口，接口要求必须实现 call 方法。
 
-type Phone interface {
-	call()
+//type Phone interface {
+//	call()
+//}
+
+//2.如何实现接口
+//如果有一个类型/结构体，实现了一个接口要求的所有方法，这里 Phone 接口只有 call方法，所以只要实现了 call 方法，我们就可以称它实现了 Phone 接口。
+//
+//意思是如果有一台机器，可以给别人打电话，那么我们就可以把它叫做电话。
+//
+//这个接口的实现是隐式的，不像 JAVA 中要用 implements 显示说明。
+//
+//继续上面电话的例子，我们先定义一个 Nokia 的结构体，而它实现了 call 的方法，所以它也是一台电话。
+
+//type Nokia struct {
+//	name string
+//}
+//
+////接收者为Nokia
+//func (phone Nokia) call() {
+//	fmt.Println("我是Nokia，是一台电话")
+//}
+
+//3.接口实现多态
+//在 Go 语言中，是通过接口来实现的多态。
+//先定义一个商品（Good）的接口，意思是一个类型或者结构体，只要实现了settleAccount() 和 orderInfo() 两个方法，那这个类型/结构体就是一个商品。
+
+type Good interface {
+	settleAccount() int
+	orderInfo() string
+}
+
+//然后我们定义两个结构体，分别是手机和赠品。
+
+type Phone struct {
+	name     string
+	quantity int
+	price    int
+}
+
+type FreeGift struct {
+	name     string
+	quantity int
+	price    int
+}
+
+//然后分别为他们实现 Good 接口的两个方法
+
+//phone
+func (phone Phone) settleAccount() int {
+	return phone.quantity * phone.price
+}
+
+func (phone Phone) orderInfo() string {
+	return "您要购买" + strconv.Itoa(phone.quantity) + "个" +
+		phone.name + "计：" + strconv.Itoa(phone.settleAccount()) + "元"
+}
+
+// FreeGift
+func (gift FreeGift) settleAccount() int {
+	return 0
+}
+func (gift FreeGift) orderInfo() string {
+	return "您要购买" + strconv.Itoa(gift.quantity) + "个" +
+		gift.name + "计：" + strconv.Itoa(gift.settleAccount()) + "元"
+}
+
+//实现了 Good 接口要求的两个方法后，手机和赠品在Go语言看来就都是商品（Good）类型了。
+//
+//这时候，我挑选了两件商品（实例化），分别是手机和耳机（赠品，不要钱）
+
+//iPhone := Phone{
+//	name: "iPhone",
+//	quantity:1,
+//	price:8000
+//}
+//earphones := FreeGift{
+//name:     "耳机",
+//quantity: 1,
+//price:    200,
+//}
+//然后创建一个购物车（也就是类型为 Good的切片），来存放这些商品。
+//goods := []Good{iPhone, earphones}
+
+//最后，定义一个方法来计算购物车里的订单金额
+func calculateAllPrice(goods []Good) int {
+	var allPrice int
+	for _, good := range goods {
+		fmt.Println(good.orderInfo())
+		allPrice += good.settleAccount()
+	}
+	return allPrice
+}
+func main() {
+	iPhone := Phone{
+		name:     "iPhone",
+		quantity: 1,
+		price:    8000,
+	}
+	earphones := FreeGift{
+		name:     "耳机",
+		quantity: 1,
+		price:    200,
+	}
+
+	goods := []Good{iPhone, earphones}
+	allPrice := calculateAllPrice(goods)
+	fmt.Printf("该订单总共需要支付 %d 元", allPrice)
+	//运行后输出如下:
+	//您要购买1个iPhone计：8000元
+	//您要购买1个耳机计：0元
+	//该订单总共需要支付 8000 元
 }
